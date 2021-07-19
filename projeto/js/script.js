24f78b45ff0925f50bcf4077bd1a686be9c60b6f
viniflorencio <viniflorenciomid@gmail.com>
2021-07-18 22:16:32 -0300

    projeto

---
+++ projeto/js/script.js
@@ -0,0 +1,7 @@
+let inputNome = document.querySelector('#nome')
+let inputEmail = document.querySelector('#email')
+let btnEnviar = document.querySelector('#enviar')
+
+btnEnviar.addEventListener('click', () => {
+    alert('Formulário enviado com sucesso!') 
+ })
